f30112695c9498e955513903b9f5d684ed90070d
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-20 14:47:59 -0500

    cadastro - recebendo e validando dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,23 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro');
+    res.render('cadastro', {validacao: {}, dadosForm: {}});
+}
+
+
+module.exports.cadastrar = function(application, req, res){
+
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome deve ter entre 5 e 20 caracteres').len(5,20);
+    req.assert('usuario', 'Usuário deve ter entre 5 e 20 caracteres').len(5,20);
+    req.assert('senha', 'Nome deve ter entre 6 e 20 caracteres').len(6,20);
+    req.assert('casa', 'Casa não pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if(erros){
+        res.render('cadastro', {validacao: erros, dadosForm: dadosForm});
+        return;
+    }
+
+    res.send('Cadastro Efetuado com Sucesso!')
 }
