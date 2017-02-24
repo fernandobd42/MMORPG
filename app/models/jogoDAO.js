2eb479f97706d3508209f21961b08565b4c620cc
fernandobd43 <fernandolucasbd@gmail.com>
2017-02-24 17:12:54 -0500

    dinamizando acoes com suditos

--- app/models/jogoDAO.js
+++ app/models/jogoDAO.js
@@ -20,12 +20,12 @@ jogoDAO.prototype.gerarParametros = function(usuario){
     });
 }
 
-jogoDAO.prototype.iniciaJogo = function(res, usuario, casa){
+jogoDAO.prototype.iniciaJogo = function(res, usuario, casa, msg){
     this._connection.open( function(err, mongoclient){
         mongoclient.collection('jogo', function(err, collection){
             collection.find({usuario: usuario}).toArray( function(err, result){
 
-                res.render('jogo', {img_casa: casa, jogo: result[0]});
+                res.render('jogo', {img_casa: casa, jogo: result[0], msg: msg});
 
                 mongoclient.close();
             });
@@ -33,6 +33,27 @@ jogoDAO.prototype.iniciaJogo = function(res, usuario, casa){
     });
 }
 
+jogoDAO.prototype.acao = function(acao){
+    this._connection.open( function(err, mongoclient){
+        mongoclient.collection('acao', function(err, collection){
+
+            var date = new Date();
+            var tempo = null;
+
+            switch(acao.acao){
+                case 1: tempo = 1 * 60 * 60000;
+                case 2: tempo = 2 * 60 * 60000;
+                case 3: tempo = 5 * 60 * 60000;
+                case 4: tempo = 5 * 60 * 60000;
+            }
+
+            acao.acao_termina_em = date.getTime() + tempo;
+            collection.insert(acao);
+
+            mongoclient.close();
+        });
+    });
+}
 
 module.exports = function(connection){
     return jogoDAO;
